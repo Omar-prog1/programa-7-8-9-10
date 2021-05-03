ec9c1277c76f2c1e7a76f8f3897fc999d3b7681f
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-03 17:34:10 -0500

    Add files via upload

---
+++ programa 9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    /*Hernandez Flores Ricardo Omar
+    equipo Zener
+    28 abr 21
+    Programa 9. Calcular Promedio de una materia    
+    */
+   float parcial1, parcial2, parcial3,calificacionfinal;
+   printf("\n\n Programa 9");
+   printf("\n\n  Calcular Promedio de una materia");
+   Printf("\n\n dame tu primer calificacion");
+   scanf("%f",&parcial1);
+   printf("\n\n dame tu segunda calificacion");
+   scanf("%f",%parcial2);
+   printf("\n\n dame tu tercer calificacion");
+   scanf("%f",parcial3);
+   calificacionfinal = (parcial1+parcial2+parcial3)/3;
+   return 0;
+}
