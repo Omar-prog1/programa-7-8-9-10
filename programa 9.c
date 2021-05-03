fc9cf5e38aa9d5811e50e66a8a2489a36b472220
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-03 18:40:33 -0500

    Update programa 9.c

--- programa 9.c
+++ programa 9.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-int main()
-{
+#include<stdio.h>
+int main(){
     /*Hernandez Flores Ricardo Omar
     equipo Zener
     28 abr 21
-    Programa 9. Calcular Promedio de una materia    
+    Programa 9 Calcular Promedio de una materia    
     */
-   float parcial1, parcial2, parcial3,calificacionfinal;
+   float cal1, cal2, cal3, prom ;
    printf("\n\n Programa 9");
    printf("\n\n  Calcular Promedio de una materia");
-   Printf("\n\n dame tu primer calificacion");
-   scanf("%f",&parcial1);
+   Printf("\ndame tu primer calificacion");
+   scanf("%f", &cal1);
    printf("\n\n dame tu segunda calificacion");
-   scanf("%f",%parcial2);
+   scanf("%f",%cal2);
    printf("\n\n dame tu tercer calificacion");
-   scanf("%f",parcial3);
-   calificacionfinal = (parcial1+parcial2+parcial3)/3;
+   scanf("%f",cal3);
+   prom= (cal1+cal2+cal)/3;
+   printf("tu promedio es : %.2f\n", prom);
+   printf("gracias por tu visita");
    return 0;
 }
