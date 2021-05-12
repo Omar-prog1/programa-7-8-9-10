34500092804c03d33ee687657aeaa0852d133641
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-12 14:13:57 -0500

    Add files via upload

---
+++ Programa10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    /*Hernandez Flores Ricardo Omar
+    equipo Zener
+    28 abr 21
+    Programa 10 Calcular el precio total con iva    
+    */
+   float precio, IVA, total;
+   printf("precio");
+   scanf("%f",&precio);
+   printf("ingresa el iva");
+   scanf("%f",&IVA);
+   total= precio+precio*(IVA/100);
+   printf("el precio final con iva es : %.2f/n", total);
+}
